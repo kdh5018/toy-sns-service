60bcc9c3c5262af1754abda172113d26b1858ed7
kdh5018 <rlaehgns745@gmail.com>
2023-11-12 01:07:46 +0900

    sns 내용 추가 기능 구현중

---
+++ toy-sns-service/Utilities/UIViewController+Popup.h
@@ -0,0 +1,16 @@
+//
+//  UIViewController+Popup.h
+//  toy-sns-service
+//
+//  Created by 김도훈 on 11/10/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (Popup)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ toy-sns-service/Utilities/UIViewController+Popup.m
@@ -0,0 +1,12 @@
+//
+//  UIViewController+Popup.m
+//  toy-sns-service
+//
+//  Created by 김도훈 on 11/10/23.
+//
+
+#import "UIViewController+Popup.h"
+
+@implementation UIViewController (Popup)
+
+@end
